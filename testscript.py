4e104cdc37f1701e2813c2c9b1075b440a6e52eb
Kitton._ <tonkittipod@gmail.com>
2022-04-20 19:26:54 +0700

    Revert "update color"
    This reverts commit c794cb3d37d846855aa3408200209f23ad68c740.

--- testscript.py
+++ testscript.py
@@ -1,5 +1,7 @@
 import requests
 import random
-res = requests.get('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all')
+res = requests.get('https://cheab-quote.herokuapp.com')
 
-print(res.json())
+num = len(res.json()['quote'])
+n = random.randint(0,num)
+print(res.json()['quote'][n])
